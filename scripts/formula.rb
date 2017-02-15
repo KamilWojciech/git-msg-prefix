88a3855f78513dbed853eb9a636b86eb67f45b08
Kamil Wojciech <kamil.wojciech@schibsted.com>
2017-02-15 19:46:32 +0100

    Update formula in homebrew-tap repo

---
+++ scripts/formula.rb
@@ -0,0 +1,35 @@
+class GitMsgPrefix < Formula
+  desc ""
+  homepage "https://github.com/KamilWojciech/git-msg-prefix"
+  url "https://github.com/KamilWojciech/git-msg-prefix/archive/{{TARBALL_NAME}}"
+  version "{{VERSION}}"
+  sha256 "{{SHA256_SUM}}"
+
+  def install
+    bin.install "bin/git-msg-prefix"
+    lib.install Dir["lib/*"]
+    FileUtils.chmod 0555, lib + 'git-hook/prepare-commit-msg'
+    homeDir = File.expand_path("~" + ENV['USER'])
+    gitHookFile = homeDir + '/.git-templates/hooks/prepare-commit-msg'
+    FileUtils.mkdir_p(gitHookFile) unless File.exists?(gitHookFile)
+
+    linked = false
+    if File.symlink?(gitHookFile)
+      symlinkTarget = File.readlink(gitHookFile)
+      if File.fnmatch File.expand_path(prefix + '../') + '**prepare-commit-msg', symlinkTarget
+        linked = true
+      end
+    end
+
+    if File.exist?(gitHookFile) && !linked
+      print "File '" + gitHookFile + "' already exists! Do you want to replace it? [Y/n]: "
+      input = STDIN.gets.strip
+      if input.downcase != 'y'
+        return false
+      end
+    end
+
+    File.delete gitHookFile
+    FileUtils.ln_s lib + 'git-hook/prepare-commit-msg', gitHookFile
+  end
+end
